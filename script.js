3f837a2277a710b507f4168f0d6362a7aacd2644
Evelin <eveliz2001@gmail.com>
2024-08-12 17:44:35 -0400

    ajustes

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-    $(".text-body-secondary").click(function() {
-      var idBoton = $("#Rio").attr("id");
-      $("#detalles" + idBoton).toggle();
-    });
-
-
-    $(".btn-close").click(function() {
-        $(".detalles").show();
-    });
+  // Al hacer clic en "Ver más", mostrar el recuadro correspondiente
+  $(".text-body-secondary").click(function() {
+      var idBoton = $(this).attr("id");
+      var detallesId = "#detalles" + idBoton;
+      
+      // Mostrar el recuadro correspondiente y ocultar los demás
+      $(".detalles").not(detallesId).hide(); // Ocultar otros recuadros
+      $(detallesId).toggle(); // Alternar visibilidad del recuadro seleccionado
   });
+
+  // Al hacer clic en el botón de cierre (X), ocultar el recuadro de detalles
+  $(".btn-close").click(function() {
+      $(this).closest(".detalles").hide();
+  });
+});
